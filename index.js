073a9fd365a8c473d533c0c30e4c25a1c40e043a
Junior <juniormbaya243@gmail.com>
2020-11-09 13:56:07 +0000

    remove port 8080

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 require("dotenv").config();
 const cors = require("cors");
 app.use(cors());
-const port = process.env.PORT_KEY || 8080;
+const port = process.env.PORT_KEY;
 
 const mongoose = require("mongoose");
 
